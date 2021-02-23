137342a18c3d8127b59ea7e8aae9d38264b9d908
User <veronika.gordynets@gmail.com>
2021-02-23 10:31:44 +0300

    Added the solution to the task from Lessons3_ClassesObjectsMethods

---
+++ Lessons3_ClassesObjectsMethods/Lessons3_ClassesObjectsMethods/IReportGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace Lessons3_ClassesObjectsMethods
+{
+    interface IReportGenerator<out TUser> : IComparer
+    {
+        public int Compare(object a, object b);
+    }
+}
