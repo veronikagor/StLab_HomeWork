a248b775f9259d6a861747ddc67775776921afd8
User <veronika.gordynets@gmail.com>
2021-04-20 00:02:00 +0300

    Add solution to the task Lessons10_Rest_API

---
+++ Lessons10_REST_API/Lessons10_REST_API/Factory/ProjectFactory.cs
@@ -0,0 +1,46 @@
+﻿using Bogus;
+using Lessons10_REST_API.Models;
+using Lessons10_REST_API.Models.ProjectModels;
+using Lessons10_REST_API.Models.TestSuiteModels;
+
+namespace Lessons10_REST_API.Factory
+{
+    public static class ProjectFactory
+    {
+        public static Client GetInvalidCredential()
+        {
+            return new Faker<Client>()
+                .RuleFor(p => p.UserName, f => f.Internet.UserName())
+                .RuleFor(p => p.Password, f => f.Internet.Password(21));
+        }
+
+        public static ProjectRequestModel GetProjectWithCorrectValues()
+        {
+            return new Faker<ProjectRequestModel>()
+                .RuleFor(p => p.Name, f => f.Lorem.Sentence(1))
+                .RuleFor(p => p.Announcement, f => f.Lorem.Sentence(3))
+                .RuleFor(p => p.ShowAnnouncement, f => f.Random.Bool())
+                .RuleFor(p => p.SuiteMode, f => f.Random.Int(1, 3));
+        }
+
+        public static ProjectRequestModel GetProjectWithMissingRequiredValues()
+        {
+            return new Faker<ProjectRequestModel>()
+                .RuleFor(p => p.Announcement, f => f.Lorem.Sentence(3))
+                .RuleFor(p => p.SuiteMode, f => f.Random.Int(1, 3));
+        }
+
+        public static TestSuiteRequestModel GetTestSuite()
+        {
+            return new Faker<TestSuiteRequestModel>()
+                .RuleFor(p => p.Name, f => f.Lorem.Sentence(1))
+                .RuleFor(p => p.Description, f => f.Lorem.Sentence(3));
+        }
+
+        public static TestSuiteRequestModel GetTestSuiteWithMissingRequiredValues()
+        {
+            return new Faker<TestSuiteRequestModel>()
+                .RuleFor(p => p.Description, f => f.Lorem.Sentence(3));
+        }
+    }
+}
---
+++ Lessons10_REST_API/Lessons10_REST_API/Steps/UserAuthorization.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Lessons10_REST_API.Factory;
+using Lessons10_REST_API.Services;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Lessons10_REST_API.Steps
+{
+    public class UserAuthorization
+    {
+        public static RestClient GetAuthorizedClient(TypeOfRights typeOfRights)
+        {
+            return typeOfRights switch
+            {
+                TypeOfRights.Admin => new RestClient()
+                {
+                    Authenticator =
+                        new HttpBasicAuthenticator(Configurator.AdminUserName, Configurator.AdminPassword)
+                },
+                TypeOfRights.User => new RestClient()
+                {
+                    Authenticator = new HttpBasicAuthenticator(Configurator.UserName, Configurator.Password)
+                },
+                _ => new RestClient()
+                {
+                    Authenticator = new HttpBasicAuthenticator(Configurator.UserName, Configurator.Password)
+                }
+            };
+        }
+
+        public static RestClient GetUnAuthorizedClient()
+        {
+            return new RestClient()
+            {
+                Authenticator = new HttpBasicAuthenticator(ProjectFactory.GetInvalidCredential().UserName,
+                    ProjectFactory.GetInvalidCredential().Password)
+            };
+        }
+    }
+}
